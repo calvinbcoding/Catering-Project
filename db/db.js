d7d5d86a06b75af8f60a5005a27b1e8648713d4e
Calvin Bruch <calvinbcoding@gmail.com>
2019-04-24 14:09:06 -0600

    Login Page and db
    Signed-off-by: Calvin Bruch <calvinbcoding@gmail.com>

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const connectionString = 'mongodb://localhost/';
+const connectionString = 'mongodb://localhost/Catering';
 
 
 mongoose.connect(connectionString, {useNewUrlParser: true,
