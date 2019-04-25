034bfe6a602b648000719a395bd566efb1dc20f8
Calvin Bruch <calvinbcoding@gmail.com>
2019-04-25 15:22:33 -0600

    Order model needed to be changed

--- models/Order.js
+++ models/Order.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const ordersSchema = new mongoose.Schema({
-    username: {type: String, required:true, unique: true},
-    password: {type: String, required: true},
+    caterer: {type: String, required:true, unique: true},
+    type: {type: String, required: true},
     user: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
