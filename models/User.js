589d2299a67311c1ce9d048f27e9c4f35fd162bf
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-04-24 20:42:15 -0600

    user delete route, mvp nav partial

--- models/User.js
+++ models/User.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-username: {type: String, required:true, unique: true},
-password: {type: String, required: true},
-order: [orderSchema],
+    username: {type: String, required:true, unique: true},
+    password: {type: String, required: true},
+    order: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order'
+    }]
 })
 
 const User = mongoose.model('User', userSchema);
